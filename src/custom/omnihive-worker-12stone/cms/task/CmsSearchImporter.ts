5dd0705ab21953c1e7cf81d406f14ca71ceb1797
Kaleb Matthews <kaleb.matthews@withone.vision>
2021-03-25 10:36:06 -0400

    Fix 12stone worker to handle access tokens

--- src/custom/omnihive-worker-12stone/cms/task/CmsSearchImporter.ts
+++ src/custom/omnihive-worker-12stone/cms/task/CmsSearchImporter.ts
@@ -16,119 +16,125 @@ export default class CmsSearchImporter extends HiveWorkerBase implements ITaskEn
             const idList: { [typeId: number]: string[] } = {};
             const processedIds: string[] = [];
 
-            const elasticWorker = this.getWorker(HiveWorkerType.Unknown, "ohElastic") as ElasticWorker;
+            const elasticWorker: ElasticWorker | undefined = this.getWorker(HiveWorkerType.Unknown, "ohElastic") as
+                | ElasticWorker
+                | undefined;
 
-            await AwaitHelper.execute(elasticWorker.init(elasticWorker.config));
+            if (elasticWorker) {
+                await AwaitHelper.execute(elasticWorker.init(elasticWorker.config));
 
-            this.graphUrl = this.config.metadata.mpGraphUrl;
+                this.graphUrl = this.config.metadata.mpGraphUrl;
 
-            const documentDataIds: { typeIds: number[]; siteIds: number[] } = await AwaitHelper.execute<{
-                typeIds: number[];
-                siteIds: number[];
-            }>(this.getDocumentTypeIds());
+                const documentDataIds: { typeIds: number[]; siteIds: number[] } = await AwaitHelper.execute<{
+                    typeIds: number[];
+                    siteIds: number[];
+                }>(this.getDocumentTypeIds());
 
-            for (const siteId of documentDataIds.siteIds) {
-                await Promise.all(
-                    documentDataIds.typeIds.map(async (typeId) => {
-                        // for (const typeId of documentDataIds.typeIds) {
+                for (const siteId of documentDataIds.siteIds) {
+                    await Promise.all(
+                        documentDataIds.typeIds.map(async (typeId) => {
+                            // for (const typeId of documentDataIds.typeIds) {
+                            console.log(
+                                chalk.yellow(
+                                    `(${dayjs().format(
+                                        "YYYY-MM-DD HH:mm:ss"
+                                    )}) Started processing => siteId: ${siteId} typeId: ${typeId}`
+                                )
+                            );
+
+                            let docList = await AwaitHelper.execute<any>(this.getFullDocuments(siteId, typeId));
+                            docList = docList?.filter(
+                                (x: any) => !processedIds.some((y: string) => y === x.DocumentId.toString())
+                            );
+
+                            if (docList && docList.length > 0) {
+                                docList.forEach((x: any) => {
+                                    if (!idList[typeId]) {
+                                        idList[typeId] = [];
+                                    }
+
+                                    idList[typeId].push(x.DocumentId.toString());
+                                });
+
+                                const chunk = 50;
+                                for (let i = 0; i < docList.length; i += chunk) {
+                                    const docChunk = docList.slice(i, i + chunk);
+
+                                    const elasticIdList: string[] = [];
+                                    docChunk.forEach((x: any) => {
+                                        for (const key in x) {
+                                            if (
+                                                !x[key] ||
+                                                key.includes("Video Attribute") ||
+                                                key.includes("Metadata") ||
+                                                key.includes("Resources")
+                                            ) {
+                                                delete x[key];
+                                                continue;
+                                            }
+
+                                            if (typeof x[key] === "number") {
+                                                continue;
+                                            }
+
+                                            if (typeof x[key] === "string") {
+                                                x[key] = x[key]
+                                                    .replace(/<[^>]*>/g, "")
+                                                    .replace(/"/g, '\\"')
+                                                    .trim();
+                                                continue;
+                                            }
+
+                                            if (dayjs(x[key]).isValid()) {
+                                                x[key] = dayjs(x[key]).format("YYYY-MM-DDThh:mm:ss");
+                                                continue;
+                                            }
+                                        }
+                                        elasticIdList.push(x.DocumentId.toString());
+                                    });
+
+                                    await AwaitHelper.execute(
+                                        elasticWorker.upsert(`cms-${typeId}`, "DocumentId", elasticIdList, docChunk)
+                                    );
+
+                                    elasticIdList.forEach((id: string) => processedIds.push(id));
+                                }
+                            }
+
+                            console.log(
+                                chalk.greenBright(
+                                    `(${dayjs().format(
+                                        "YYYY-MM-DD HH:mm:ss"
+                                    )}) Completed processing => siteId: ${siteId} typeId: ${typeId}`
+                                )
+                            );
+                        })
+                    );
+                }
+
+                if (elasticWorker.client && Object.keys(idList).length > 0) {
+                    for (const typeId in idList) {
                         console.log(
-                            chalk.yellow(
-                                `(${dayjs().format(
-                                    "YYYY-MM-DD HH:mm:ss"
-                                )}) Started processing => siteId: ${siteId} typeId: ${typeId}`
+                            chalk.gray(
+                                `(${dayjs().format("YYYY-MM-DD HH:mm:ss")}) Removing unused Ids => typeId: ${typeId}`
                             )
                         );
 
-                        let docList = await AwaitHelper.execute<any>(this.getFullDocuments(siteId, typeId));
-                        docList = docList?.filter(
-                            (x: any) => !processedIds.some((y: string) => y === x.DocumentId.toString())
+                        await AwaitHelper.execute(
+                            elasticWorker.removeUnused("cms-" + typeId, "DocumentId", idList[typeId])
                         );
-
-                        if (docList && docList.length > 0) {
-                            docList.forEach((x: any) => {
-                                if (!idList[typeId]) {
-                                    idList[typeId] = [];
-                                }
-
-                                idList[typeId].push(x.DocumentId.toString());
-                            });
-
-                            const chunk = 50;
-                            for (let i = 0; i < docList.length; i += chunk) {
-                                const docChunk = docList.slice(i, i + chunk);
-
-                                const elasticIdList: string[] = [];
-                                docChunk.forEach((x: any) => {
-                                    for (const key in x) {
-                                        if (
-                                            !x[key] ||
-                                            key.includes("Video Attribute") ||
-                                            key.includes("Metadata") ||
-                                            key.includes("Resources")
-                                        ) {
-                                            delete x[key];
-                                            continue;
-                                        }
-
-                                        if (typeof x[key] === "number") {
-                                            continue;
-                                        }
-
-                                        if (typeof x[key] === "string") {
-                                            x[key] = x[key]
-                                                .replace(/<[^>]*>/g, "")
-                                                .replace(/"/g, '\\"')
-                                                .trim();
-                                            continue;
-                                        }
-
-                                        if (dayjs(x[key]).isValid()) {
-                                            x[key] = dayjs(x[key]).format("YYYY-MM-DDThh:mm:ss");
-                                            continue;
-                                        }
-                                    }
-                                    elasticIdList.push(x.DocumentId.toString());
-                                });
-
-                                await AwaitHelper.execute(
-                                    elasticWorker.upsert(`cms-${typeId}`, "DocumentId", elasticIdList, docChunk)
-                                );
-
-                                elasticIdList.forEach((id: string) => processedIds.push(id));
-                            }
-                        }
 
                         console.log(
                             chalk.greenBright(
                                 `(${dayjs().format(
                                     "YYYY-MM-DD HH:mm:ss"
-                                )}) Completed processing => siteId: ${siteId} typeId: ${typeId}`
+                                )}) Completed removing unused Ids => typeId: ${typeId}`
                             )
                         );
-                    })
-                );
-            }
-
-            if (elasticWorker.client && Object.keys(idList).length > 0) {
-                for (const typeId in idList) {
-                    console.log(
-                        chalk.gray(
-                            `(${dayjs().format("YYYY-MM-DD HH:mm:ss")}) Removing unused Ids => typeId: ${typeId}`
-                        )
-                    );
-
-                    await AwaitHelper.execute(
-                        elasticWorker.removeUnused("cms-" + typeId, "DocumentId", idList[typeId])
-                    );
-
-                    console.log(
-                        chalk.greenBright(
-                            `(${dayjs().format(
-                                "YYYY-MM-DD HH:mm:ss"
-                            )}) Completed removing unused Ids => typeId: ${typeId}`
-                        )
-                    );
+                    }
                 }
+            } else {
+                throw new Error("Failed to find an elastic worker");
             }
 
             return;

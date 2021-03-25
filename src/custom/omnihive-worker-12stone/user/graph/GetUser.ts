1cbf7202dd22bf0bd8d9d372ae7000389a622ce5
Kaleb Matthews <kaleb.matthews@withone.vision>
2021-03-25 16:19:25 -0400

    Get User Dany Wrapper added

---
+++ src/custom/omnihive-worker-12stone/user/graph/GetUser.ts
@@ -0,0 +1,22 @@
+import { AwaitHelper } from "@withonevision/omnihive-core/helpers/AwaitHelper";
+import { IGraphEndpointWorker } from "@withonevision/omnihive-core/interfaces/IGraphEndpointWorker";
+import { HiveWorkerBase } from "@withonevision/omnihive-core/models/HiveWorkerBase";
+import { serializeError } from "serialize-error";
+import { danyGet } from "../../lib/helpers/DanyHelper";
+import DanyService from "../../lib/services/DanyService";
+
+export default class ForgotPassword extends HiveWorkerBase implements IGraphEndpointWorker {
+    public execute = async (customArgs: any): Promise<any> => {
+        try {
+            // Get Metadata
+            DanyService.getSingleton().setMetaData(this.config.metadata);
+
+            const result = await AwaitHelper.execute(danyGet("/Users/GetUser", customArgs.authToken));
+
+            return result.data;
+        } catch (err) {
+            console.log(JSON.stringify(serializeError(err)));
+            return err;
+        }
+    };
+}

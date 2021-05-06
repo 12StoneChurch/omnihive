71b20bf7efd02320a3e142fcc3f42e310e706ed4
Kaleb Matthews <kaleb.matthews@withone.vision>
2021-05-06 14:48:37 -0400

    Move FusionAuth get user call to auth worker

--- src/custom/omnihive-worker-fusionauth/lib/models/MpContact.ts
+++
@@ -1,9 +0,0 @@
-export type MpContact = {
-    birthDate: string;
-    email: string;
-    firstName: string;
-    nickname: string;
-    lastName: string;
-    middleName: string;
-    mobilePhone: string;
-};

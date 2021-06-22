61e4bd581cd28118ef4d5ab2b4d44a832befa5a6
Kaleb Matthews <kaleb.matthews@withone.vision>
2021-06-22 11:52:11 -0400

    feat: image workers now take a search function
    Add the ability to search for images in an ImageWorker

---
+++ src/custom/omnihive-worker-12stone-media/graph/imageSearch.ts
@@ -0,0 +1,45 @@
+import { IGraphEndpointWorker } from "@withonevision/omnihive-core/interfaces/IGraphEndpointWorker";
+import { HiveWorkerBase } from "@withonevision/omnihive-core/models/HiveWorkerBase";
+import { serializeError } from "serialize-error";
+import { IImageWorker } from "@12stonechurch/omnihive-worker-common/interfaces/IImageWorker";
+
+/**
+ * Args:
+ * 
+ *  searchExpression: string
+ *  sortBy: {key: string, orderBy: "asc" | "desc"}
+ *  maxResults: number
+ *  nextCursor: string
+ *  withFields: string
+ *  aggregate: string
+ * 
+ */
+export default class ImageSearch extends HiveWorkerBase implements IGraphEndpointWorker {
+    public execute = async (customArgs: any): Promise<any> => {
+        try {
+            // Validate arguments
+            if (!customArgs.searchExpression) {
+                throw new Error("A searchExpression is required.");
+            }
+
+            const imageWorker: IImageWorker | undefined = this.getWorker("image");
+
+            if (imageWorker) {
+                const results = await imageWorker.search(
+                    customArgs.searchExpression, 
+                    customArgs.sortBy, 
+                    customArgs.maxResults, 
+                    customArgs.nextCursor, 
+                    customArgs.withFields, 
+                    customArgs.aggregate
+                );
+                return { results };
+            }
+
+            return {results: "No results found"};
+        } catch (err) {
+            console.log(JSON.stringify(serializeError(err)));
+            return err;
+        }
+    };
+}

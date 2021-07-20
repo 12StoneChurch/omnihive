d210f6670c9339ded564d9cf5af488cc77d8c97b
Clayton Ingalls <clayton.ingalls@withonevision.com>
2021-07-20 11:58:04 -0400

    refactor(engagements worker): :recycle: returns counts grouped by engagement status

--- src/custom/omnihive-worker-12stone-engagements/graph/GetEngagementsCount.ts
+++ src/custom/omnihive-worker-12stone-engagements/graph/GetEngagementsCount.ts
@@ -1,11 +1,10 @@
+import { HiveWorkerType } from "@withonevision/omnihive-core/enums/HiveWorkerType";
+import { IDatabaseWorker } from "@withonevision/omnihive-core/interfaces/IDatabaseWorker";
 import { IGraphEndpointWorker } from "@withonevision/omnihive-core/interfaces/IGraphEndpointWorker";
 import { GraphContext } from "@withonevision/omnihive-core/models/GraphContext";
 import { HiveWorkerBase } from "@withonevision/omnihive-core/models/HiveWorkerBase";
 import { Knex } from "knex";
 import { serializeError } from "serialize-error";
-
-import { HiveWorkerType } from "@withonevision/omnihive-core/enums/HiveWorkerType";
-import { IDatabaseWorker } from "@withonevision/omnihive-core/interfaces/IDatabaseWorker";
 
 // import { EngagementModel } from "../lib/models/Engagement";
 // import { countQueryBuilder } from "../queries/countQueryBuilder";
@@ -14,7 +13,14 @@ interface Args {
     ownerId: number;
 }
 
-export default class GetAllEngagements extends HiveWorkerBase implements IGraphEndpointWorker {
+interface Statuses {
+    New?: number;
+    Open?: number;
+    Snoozed?: number;
+    Closed?: number;
+}
+
+export default class GetEngagmentsCount extends HiveWorkerBase implements IGraphEndpointWorker {
     public execute = async (customArgs: Args, _omniHiveContext: GraphContext): Promise<{}> => {
         try {
             if (!customArgs?.ownerId) {
@@ -33,9 +39,13 @@ export default class GetAllEngagements extends HiveWorkerBase implements IGraphE
             const countQuery = countQueryBuilder(connection, ownerId);
             const res = await worker?.executeQuery(countQuery.toString());
 
-            const data = res && res[0][0];
+            const data = res && res[0];
+            const countObj: Statuses = {};
+            data?.forEach((item: { Name: "New" | "Open" | "Snoozed" | "Closed"; count: number }) => {
+                countObj[item.Name] = item.count;
+            });
 
-            return data;
+            return countObj;
         } catch (err) {
             console.log(JSON.stringify(serializeError(err)));
             return err;
@@ -46,7 +56,13 @@ export default class GetAllEngagements extends HiveWorkerBase implements IGraphE
 const countQueryBuilder = (connection: Knex, ownerId: number) => {
     const builder = connection.queryBuilder();
 
-    builder.count("Engagement_ID as count").from("Engagements").where("Owner_Contact_ID", ownerId);
+    builder
+        .select("es.Name")
+        .count("e.Engagement_ID as count")
+        .from("Engagements as e")
+        .innerJoin("Engagement_Statuses as es", "e.Engagement_Status_ID", "es.Engagement_Status_ID")
+        .where("Owner_Contact_ID", ownerId)
+        .groupBy("es.Name");
 
     return builder;
 };

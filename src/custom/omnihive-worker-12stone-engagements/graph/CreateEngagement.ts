a39bb24e0577331fe8704b9f29fcd9dcf26ab49f
Clayton Ingalls <clayton.ingalls@withonevision.com>
2021-07-26 15:19:17 -0400

    fix(engagements worker): returns engagement object instead of array on engagement creation

--- src/custom/omnihive-worker-12stone-engagements/graph/CreateEngagement.ts
+++ src/custom/omnihive-worker-12stone-engagements/graph/CreateEngagement.ts
@@ -105,7 +105,7 @@ export default class CreateEngagement extends HiveWorkerBase implements IGraphEn
                     trx.rollback;
                 }
 
-                return engagementData;
+                return engagementData[0];
             });
 
             return engagement;

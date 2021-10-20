756715c190ebfd1e10dab63f1b413f87a963603a
Kaleb Matthews <kaleb.matthews@withone.vision>
2021-10-20 13:31:08 -0400

    fix(worker): fix notifications being sent to all group leaders
    Only send notifications to leaders that have attendance enabled

--- src/custom/omnihive-worker-12stone-groups/queries/getAllGroupLeaders.ts
+++ src/custom/omnihive-worker-12stone-groups/queries/getAllGroupLeaders.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import weekday from "dayjs/plugin/weekday";
 import { Knex } from "knex";
+import { IDatabaseWorker } from "@withonevision/omnihive-core/interfaces/IDatabaseWorker";
 
 import { NotificationSummary } from "../models/Notification";
 
@@ -43,33 +44,43 @@ interface GroupParticipantsDTO {
 }
 
 interface AllGroupLeadersGetter {
-    (knex: Knex): Promise<NotificationSummary[]>;
+    (databaseWorker: IDatabaseWorker, attendanceOnly?: boolean): Promise<NotificationSummary[]>;
 }
 
-export const getAllGroupLeaders: AllGroupLeadersGetter = async (knex: Knex) => {
-    const result = (await knex
-        .distinct([
-            "g.group_id",
-            "c.contact_id",
-            "g.group_name",
-            "g.meeting_day_id",
-            "md.meeting_day",
-            "g.meeting_time",
-            "c.mobile_phone",
-        ])
-        .from("group_participants as gp")
-        .leftJoin("group_roles as gr", "gp.group_role_id", "gr.group_role_id")
-        .leftJoin("groups as g", "gp.group_id", "g.group_id")
-        .leftJoin("group_statuses as gs", "g.group_status_id", "gs.group_status_id")
-        .leftJoin("meeting_days as md", "g.meeting_day_id", "md.meeting_day_id")
-        .leftJoin("contacts as c", "gp.participant_id", "c.participant_record")
-        .whereNotNull("g.meeting_day_id")
-        .and.whereNotNull("g.meeting_time")
-        .and.whereRaw("isnull(g.end_date, '1/1/2100') >= getdate()")
-        .and.whereRaw("isnull(gp.end_date, '1/1/2100') >= getdate()")
-        .and.where("gr.role_title", "Leader")
-        .and.whereNotNull("c.mobile_phone")
-        .and.where("c.do_not_text", 0)) as GroupParticipantsDTO[];
+export const getAllGroupLeaders: AllGroupLeadersGetter = async (
+    databaseWorker: IDatabaseWorker,
+    attendanceOnly?: boolean
+) => {
+    const knex: Knex = databaseWorker.connection;
+    const queryBuilder: Knex.QueryBuilder<any, any> = knex.queryBuilder();
+    queryBuilder.distinct([
+        "g.group_id",
+        "c.contact_id",
+        "g.group_name",
+        "g.meeting_day_id",
+        "md.meeting_day",
+        "g.meeting_time",
+        "c.mobile_phone",
+    ]);
+    queryBuilder.from("group_participants as gp");
+    queryBuilder.leftJoin("group_roles as gr", "gp.group_role_id", "gr.group_role_id");
+    queryBuilder.leftJoin("groups as g", "gp.group_id", "g.group_id");
+    queryBuilder.leftJoin("group_statuses as gs", "g.group_status_id", "gs.group_status_id");
+    queryBuilder.leftJoin("meeting_days as md", "g.meeting_day_id", "md.meeting_day_id");
+    queryBuilder.leftJoin("contacts as c", "gp.participant_id", "c.participant_record");
+    queryBuilder.whereNotNull("g.meeting_day_id");
+    queryBuilder.andWhereRaw("g.meeting_time is not null");
+    queryBuilder.andWhereRaw("isNull(g.end_date, '1/1/2100') >= getDate()");
+    queryBuilder.andWhereRaw("isNull(gp.end_date, '1/1/2100') >= getDate()");
+    queryBuilder.andWhere("gr.role_title", "Leader");
+    queryBuilder.andWhereRaw("c.mobile_phone is null");
+    queryBuilder.andWhere("c.do_not_text", 0);
+
+    if (attendanceOnly) {
+        queryBuilder.andWhere("g.Enable_Attendance", 1);
+    }
+
+    const result = (await databaseWorker.executeQuery(queryBuilder.toString()))[0] as GroupParticipantsDTO[];
 
     return result.map((row) => {
         return {
